0bf022f6ddbb055428443aa0f6c0966c3085662b
thelovemime <53869601+thelovemime@users.noreply.github.com>
2019-08-28 09:29:13 -0500

    Update centuryLoop.py

--- centuryLoop.py
+++ centuryLoop.py
@@ -7,7 +7,7 @@ c = 0
 while c < 100:
 	num = int(input("Ingresa la base del cuadrado: "))
 	num2 = int(input("ingresa la su altura: "))
-	print("resultado: ", num+num2,"\nquieres volver a calcular?")
+	print("resultado: ", num*num2,"\nquieres volver a calcular?")
 	resp = input()
 	
 	if resp == "si":
